71797b81f28fc6bf4645366e86cbc31bdf75916f
SantySantos <santysantos533@gmail.com>
2025-09-23 14:37:31 -0400

    finishing main

--- Assignment1/JavaClass - Assignment01/src/Main.java
+++ Assignment1/JavaClass - Assignment01/src/Main.java
@@ -4,7 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         //Bird extends Animal implements IFlightEnabled, ITrackable
+        goBird();
 
+        //Satellite implements OrbitEarth which implements FlightEnabled
+        goSatellite();
+
+        //gathering all entities implementing the ITrackable interface
+        goTrackables();
     }
 
     private static void goNext() {
@@ -21,14 +27,49 @@ public class Main {
                 new Bird(),
                 new Truck(),
                 new Jet(),
-        }
+        };
 
-        
+        for(ITrackable trackable :trackables){
+            trackable.track();
+
+            goNext();
+        }
     }
 
-    private static void goBird() {
+    private static void goSatellite(){
+
+        //Satellite implements IOrbitEarth which implements IFlightEnabled
+        Satellite satellite = new Satellite();
+        satellite.achieveOrbit();
+        satellite.takeOff();
+        satellite.fly();
+        satellite.land();
+
+        IOrbitEarth orbitEarth = satellite;
+        //orbitEarth and satellite pointing to the same object (copy)
+        System.out.println(satellite == orbitEarth);
+        orbitEarth.achieveOrbit();
+        //downcasting gives access to the Satellite object.
+        ((Satellite) orbitEarth).land();
+        goNext();
+
+    }
+
+    private static void goBird(){
 
         //Bird extends Animal implements FlightEnabled, Trackable
+
+        Bird bird = new Bird();
+        Animal animal = bird;
+
+        //downcasting animal
+        ((Bird)animal).track();
+
+        IFlightEnabled  flier = bird; //flier has access to the 3 methods of FlightEnabled.
+
+        ITrackable tracked = bird;//only access to track() method from Trackable type (interface)
+
+        goNext();
 
     }
 }
