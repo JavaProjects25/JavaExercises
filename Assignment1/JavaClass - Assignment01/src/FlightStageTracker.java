3fdc25826e08407f5986229bef7ebd3e7584d76f
SantySantos <santysantos533@gmail.com>
2025-09-23 13:55:26 -0400

    implementing inheritance and interfaces

--- Assignment1/JavaClass - Assignment01/src/FlightStageTracker.java
+++ Assignment1/JavaClass - Assignment01/src/FlightStageTracker.java
@@ -1,10 +1,11 @@
-class FlightStageTracker {
+class FlightStageTracker implements ITrackable {
 
     private FlightStage stage;
     FlightStageTracker(FlightStage stage){
         this.stage = stage;
     }
 
+    @Override
     public void track(){
         if(stage != FlightStage.GROUNDED){
             System.out.println("Monitoring " + stage);
