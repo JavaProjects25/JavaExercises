203299f194dfa11e71d7cc699e6c4d02c94cb74b
SantySantos <santysantos533@gmail.com>
2025-09-23 14:25:13 -0400

    deleting bird class

--- Assignment1/JavaClass - Assignment01/src/Main.java
+++ Assignment1/JavaClass - Assignment01/src/Main.java
@@ -2,8 +2,33 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+
+        //Bird extends Animal implements IFlightEnabled, ITrackable
+
+    }
+
+    private static void goNext() {
+        System.out.println("\n\n-------------\n\n");
+    }
+
+    private static void goTrackables() {
+
+
+        //gathering all entities implementing the ITrackable interface
+
+        //FlightStages flightStages
+        ITrackable[] trackables = {
+                new Bird(),
+                new Truck(),
+                new Jet(),
+        }
+
+        
+    }
+
+    private static void goBird() {
+
+        //Bird extends Animal implements FlightEnabled, Trackable
+
     }
 }
