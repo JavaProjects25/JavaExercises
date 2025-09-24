d30e04498b0f61e43c5de532f4bcf259ebd5e735
SantySantos <santysantos533@gmail.com>
2025-09-23 20:31:00 -0400

    updating main

--- Assignment1/JavaClass - Assignment01/src/Main.java
+++ Assignment1/JavaClass - Assignment01/src/Main.java
@@ -11,6 +11,11 @@ public class Main {
 
         //gathering all entities implementing the ITrackable interface
         goTrackables();
+
+        //doing downcasting with AlienShip which implements multiple interfaces
+        goAlienShip();
+
+        goReconDrone();
     }
 
     private static void goNext() {
@@ -27,16 +32,28 @@ public class Main {
                 new Bird(),
                 new Truck(),
                 new Jet(),
+                new Truck(),
+                new Bee(),
+                new Dolphin(),
+                new AlienShip(),
+                new AutonomousBoat(),
+                new Drone(),
+                new Missile(),
+                new ReconDrone(),
+                new SelfDrivingCar(),
+                new SpaceProbe(),
+                new UFO(),
+                new WeatherBalloon(),
         };
 
-        for(ITrackable trackable :trackables){
+        for (ITrackable trackable : trackables) {
             trackable.track();
 
             goNext();
         }
     }
 
-    private static void goSatellite(){
+    private static void goSatellite() {
 
         //Satellite implements IOrbitEarth which implements IFlightEnabled
         Satellite satellite = new Satellite();
@@ -55,7 +72,7 @@ public class Main {
 
     }
 
-    private static void goBird(){
+    private static void goBird() {
 
         //Bird extends Animal implements FlightEnabled, Trackable
 
@@ -63,11 +80,44 @@ public class Main {
         Animal animal = bird;
 
         //downcasting animal
-        ((Bird)animal).track();
+        ((Bird) animal).track();
 
-        IFlightEnabled  flier = bird; //flier has access to the 3 methods of FlightEnabled.
+        IFlightEnabled flier = bird; //flier has access to the 3 methods of FlightEnabled.
 
         ITrackable tracked = bird;//only access to track() method from Trackable type (interface)
+
+        goNext();
+
+    }
+
+    private static void goAlienShip() {
+
+        //Deploying alien ship utilities
+        AlienShip alienShip = new AlienShip();
+        alienShip.takeOff();
+        alienShip.fly();
+        alienShip.captureImage();
+        alienShip.deployWeapon();
+        alienShip.setDestination("Mars");
+        alienShip.engageAutopilot();
+        System.out.println("Energy type: " + alienShip.getEnergyType());
+
+        //downcasting to ITrackable interface
+        ITrackable tracker = alienShip;
+        tracker.track();
+        ((AlienShip) tracker).land();
+        goNext();
+    }
+
+    private static void goReconDrone() {
+        ReconDrone reconDrone = new ReconDrone();
+        Drone drone = reconDrone;
+        reconDrone.takeOff();
+        reconDrone.fly();
+        reconDrone.captureImage();
+        reconDrone.land();
+
+        ((ReconDrone) drone).readSensors();
 
         goNext();
 
