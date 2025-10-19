584fdbabc2e9107da66ab2f4424d881ba7b47ede
SantySantos <santysantos533@gmail.com>
2025-10-18 21:43:05 -0400

    bro

--- Assignment4/src/Main.java
+++ Assignment4/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         //Initialization
         DesertCondoBuilder desertCondoBuilder = new DesertCondoBuilder();
         Condo.Builders.Engineer desertCondoEngineer = new Condo.Builders.Engineer(desertCondoBuilder);
@@ -40,6 +41,9 @@ public class Main {
         desertHouseEngineer.getHouse().displayHouse();
         iglooHouseEngineer.getHouse().displayHouse();
         jungleHouseEngineer.getHouse().displayHouse();
+=======
+
+>>>>>>> parent of f7c88c0 (Update draw.io diagram typo)
 =======
 
 >>>>>>> parent of f7c88c0 (Update draw.io diagram typo)
